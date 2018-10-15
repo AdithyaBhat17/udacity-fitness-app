e4331c2850cf84136589f17ceb3d41710fff88a7
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2018-10-15 13:03:55 +0530

    Feat : Created store and provided it to AddEntry.js

--- App.js
+++ App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Ionicons, MaterialIcons } from '@expo/vector-icons';
 import AddEntry from './components/AddEntry';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import Reducer from './reducers';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <AddEntry/>
-      </View>
+      <Provider store={createStore(Reducer)}>
+        <View>
+          <AddEntry/>
+        </View>
+      </Provider>
     );
   }
 }
